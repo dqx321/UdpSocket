9c9810f57703967ee423f8cd6c107aab5b8567aa
dai <dai.qx@cidi.ai>
2019-09-25 15:31:34 +0800

    a

--- app/src/main/java/com/cidi/udpsocket/UdpTest.java
+++ app/src/main/java/com/cidi/udpsocket/UdpTest.java
@@ -10,5 +10,9 @@ public class UdpTest {
     public static void getStr() {
         Log.e("udpTest", "调取jar的方法");
 
+    }public static void getMyStr() {
+        Log.e("udpTest", "调取getMyStrjar的方法");
+
     }
+
 }
