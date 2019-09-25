c555b759297abb087782995376ac1780b33d1c0a
dai <dai.qx@cidi.ai>
2019-09-25 16:48:03 +0800

    .

---
+++ radarmapview/src/main/java/com/example/radarmapview/Test.java
@@ -0,0 +1,14 @@
+package com.example.radarmapview;
+
+import android.util.Log;
+
+/**
+ * @author CIDI
+ * on 2019/9/25.
+ */
+public class Test {
+
+    public void testLog(){
+        Log.e("testLog","测试打印日志");
+    }
+}
